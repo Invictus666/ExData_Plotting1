974b0bc453050543b95e1ab59bd7c0fe2bb7d2fa
Christopher Ng Wai Chung <waichung.ng@gmail.com>
2014-05-06 15:32:48 +0800

    Main File

---
+++ Power_Consumption.R
@@ -0,0 +1,17 @@
+
+getdata <- function()
+{
+data <- read.csv("household_power_consumption.txt",sep=";",stringsAsFactors = FALSE)
+subdata <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007" ,]
+subdata$DateTime <- paste(subdata$Date,subdata$Time)
+subdata$DateTime <- strptime(subdata$DateTime,"%d/%m/%Y %H:%M:%S")
+subdata$Global_active_power <- as.numeric(subdata$Global_active_power)
+subdata$Global_reactive_power <- as.numeric(subdata$Global_reactive_power)
+subdata$Voltage <- as.numeric(subdata$Voltage)
+subdata$Global_intensity <- as.numeric(subdata$Global_intensity)
+subdata$Sub_metering_1 <- as.numeric(subdata$Sub_metering_1)
+subdata$Sub_metering_2 <- as.numeric(subdata$Sub_metering_2)
+subdata$Sub_metering_3 <- as.numeric(subdata$Sub_metering_3)
+subdata
+}
+
